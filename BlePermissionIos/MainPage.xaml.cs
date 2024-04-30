1f59fde2c262f48d742d3d5ce02039d9a6ee2362
chuckkrutsinger <chuck.krutsinger@ksoftllc.com>
2024-04-29 20:30:56 -0600

    initial commit

---
+++ BlePermissionIos/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace BlePermissionIos;
+
+public partial class MainPage : ContentPage
+{
+    private IAdapter _adapter;
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        _adapter = CrossBluetoothLE.Current.Adapter;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Task.Run(async () =>
+		{
+			var status = await Permissions.CheckStatusAsync<Permissions.Bluetooth>();
+			await MainThread.InvokeOnMainThreadAsync(() =>
+			{
+				CounterBtn.Text = $"{status}";
+			});
+		});
+	}
+}
+
